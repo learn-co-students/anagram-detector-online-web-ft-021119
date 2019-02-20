57f3caee3533eede2184ab620126f3fae866e324
Joan Lyness <joanrigdon@gmail.com>
2019-02-20 23:36:04 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+
+def initialize(word)
+  @word = word
+end
+
+def match(possible_anagrams)
+  match = []
+  possible_anagrams.each do |poss|
+    if poss.chars.sort.join == @word.chars.sort.join
+      match << poss
+    end
+  end
+  match
+end
+
+end
