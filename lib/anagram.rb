f24c38d2f55468335d36ce9f12108d035b80c0e5
Vinh Do <vinhdo07@gmail.com>
2019-02-22 00:51:22 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+  attr_accessor :word
+  @@all = []
+
+  def initialize(word)
+    @word = word.split(//).sort.join("")
+  end
+
+  def match(array)
+    @@all.clear
+    array.each do |word_element|
+      x = word_element.split(//).sort.join("")
+      @@all << word_element if x == @word
+    end
+    @@all
+  end
+end
