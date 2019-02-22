e411f339a288b29e31b9e5f05ff489f205a06d1a
Burak Kocak <burakkocak884@ghotmail.com>
2019-02-22 16:45:39 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,48 @@
+require 'pry'
+class Anagram
+@@all = []
+attr_accessor :word
+
+def initialize(word)
+
+	@word = word
+#@@all << word.match(%w(self)
+end
+ def match(name)
+  
+   match_list = []
+   original_word = @word.split("").sort!
+		original_sorted = original_word.join("")
+ #binding.pry
+ 	name.each_with_index do |words, index|
+ 		#binding.pry
+ 		 split_words = words.split("")
+ 		split_words.sort!
+ 		word_sorted = split_words.join("")
+	
+ 		 if original_sorted.match(word_sorted)
+ 		 	match_list << words
+ 			 end
+ 			#binding.pry
+
+
+ 			
+ 			
+ 				#binding.pry
+ 	end
+ 	match_list
+ 	end	
+
+    
+ 	     
+ 	#binding.pry
+ 	
+ 	#	binding.pry
+ 	
+
+
+
+end
+
+
 # Your code goes here!
