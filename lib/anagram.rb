0ff28a49fea23e77872be38b7a4b86960cd1a3e9
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-03-05 01:19:26 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
-# Your code goes here!
+require 'pry'
+# Your class, Anagram should take a word on initialization, and instances should respond to a match method that takes an array of possible anagrams. It should return all matches in an array. If no matches exist, it should return an empty array.
+
+# In other words, given: "listen" and %w(enlists google inlets banana) the program should return ["inlets"].
+class Anagram
+  attr_accessor :word
+  def initialize(word)
+    @word = word
+    end
+# Does an array of letters (@word) match another array of letters(array)?
+  def match(array)                    # ["hello", "world", "zombies", "pants", "dipper"]
+    split_word = @word.split("").sort # ["a", "d", "e", "i", "p", "r"]
+                                      # @word.match(%w(array))
+                                      # (element.split("").sort)
+  end
+end
+                                      # An example:
+                                      # listen = Anagram.new("listen")
+                                      # listen.match(%w(enlists google inlets banana))
+                                      #
+                                      # # => ["inlets"]
+                                      # %w(@word)
